bc2a87570b36926107c7807c21bbffa355e515dd
XxxxxMing <windlessg@gmail.com>
2018-10-12 17:01:24 +0800

    新增 index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { exec } = require('child_process')
+const path = require('path')
+const os = require('os')
+
+function detectQrCode(file) {
+  return new Promise((resolve) => {
+    const bin = path.join(__dirname, `bin/${os.platform()}/inspect`)
+    exec(`${bin} ${file}`, (err, stdout, stderr) => {
+      console.log(stdout)
+      console.log(stderr)
+      if (stderr) {
+        resolve(false)
+      } else {
+        resolve(true)
+      }
+    })
+  })
+}
+
+module.exports = detectQrCode
